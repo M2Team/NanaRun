b7244110db55d90ee20d8e3df236b5216f1aac9b
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-05-17 11:59:46 +0800

    Add NanaRunConsole project.

---
+++ NanaRunConsole/NanaRunConsole.cpp
@@ -0,0 +1,18 @@
+﻿/*
+ * PROJECT:   NanaRun
+ * FILE:      NanaRunConsole.cpp
+ * PURPOSE:   Implementation for NanaRun (Console)
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include <cwchar>
+
+int main()
+{
+    std::wprintf(L"Hello World! - NanaRun (Console)");
+
+    return 0;
+}
