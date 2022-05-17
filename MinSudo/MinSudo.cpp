ab0d709598316dc7e381ae19d10847998844ac36
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-05-18 06:41:59 +0800

    Add MinSudo project.

---
+++ MinSudo/MinSudo.cpp
@@ -0,0 +1,18 @@
+﻿/*
+ * PROJECT:   NanaRun
+ * FILE:      MinSudo.cpp
+ * PURPOSE:   Implementation for MinSudo
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include <cwchar>
+
+int main()
+{
+    std::wprintf(L"Hello World! - MinSudo");
+
+    return 0;
+}
