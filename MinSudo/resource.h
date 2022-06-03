5e94629ea862dcf423e40e0eeb93e51fba91bd23
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-06-03 11:35:55 +0800

    Add icon for MinSudo.

--- MinSudo/resource.h
+++ MinSudo/resource.h
@@ -4,12 +4,13 @@
 //
 #define IDR_TRANSLATIONS                101
 #define IDR_TRANSLATIONS1               102
+#define IDI_MINSUDO                     103
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
