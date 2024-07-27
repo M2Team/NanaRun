84fa6c25db4861721c371404707ac7c5d5aa851a
MouriNaruto <Kenji.Mouri@outlook.com>
2024-07-27 21:30:00 +0800

    Introduce SynthRdp project.

---
+++ SynthRdp/SynthRdp.cpp
@@ -0,0 +1,26 @@
+﻿/*
+ * PROJECT:   NanaRun
+ * FILE:      SynthRdp.cpp
+ * PURPOSE:   Implementation for Hyper-V VMBus Enhanced Session Service
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <cstdint>
+
+#include <cstdio>
+#include <cwchar>
+
+int main()
+{
+    std::wprintf(
+        L"Hyper-V VMBus Enhanced Session Service\n"
+        L"================================================================\n"
+        L"Hello World!\n");
+
+    std::getchar();
+
+    return 0;
+}
