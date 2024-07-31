233c89a9c9281671f91b3133920ca9f2a37b6938
MouriNaruto <Kenji.Mouri@outlook.com>
2024-08-01 00:29:36 +0800

    Add NanaRun.IconResource.

---
+++ NanaRun.IconResource/NanaRun.IconResource.h
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   NanaRun
+ * FILE:      NanaRun.IconResource.h
+ * PURPOSE:   Windows icon resource for NanaRun
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#define IDI_NANARUN                       101
