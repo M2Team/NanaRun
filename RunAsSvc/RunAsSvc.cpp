66203997f8b4d499bd02debc7361b11a42f468de
MouriNaruto <Kenji.Mouri@outlook.com>
2026-05-11 00:27:21 +0800

    Add RunAsSvc project.

---
+++ RunAsSvc/RunAsSvc.cpp
@@ -0,0 +1,18 @@
+﻿/*
+ * PROJECT:    NanaRun
+ * FILE:       RunAsSvc.cpp
+ * PURPOSE:    Implementation for Run As Windows Service Utility
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <Mile.Helpers.CppBase.h>
+
+int main()
+{
+    std::wprintf(L"Hello World! - RunAsSvc");
+
+    return 0;
+}
