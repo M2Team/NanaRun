50f6b9f7316c817fa1606cbc752fd4b6e9a5c121
MouriNaruto <Kenji.Mouri@outlook.com>
2026-05-11 01:06:55 +0800

    Add some notes to RunAsSvc.

--- RunAsSvc/RunAsSvc.cpp
+++ RunAsSvc/RunAsSvc.cpp
@@ -10,6 +10,28 @@
 
 #include <Mile.Helpers.CppBase.h>
 
+// RunAsSvc Install [ServiceName] [Options]
+// RunAsSvc Uninstall [ServiceName]
+// RunAsSvc Start [ServiceName]
+// RunAsSvc Stop [ServiceName]
+// RunAsSvc Enable [ServiceName]
+// RunAsSvc Disable [ServiceName]
+
+// Private
+// RunAsSvc Instance [ServiceName]
+
+// HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\[ServiceName]
+// HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services[ServiceName]\RunAsSvc
+// - REG_SZ: ApplicationName
+// - REG_SZ: CommandLine
+// - REG_SZ: CurrentDirectory
+// - REG_MULTI_SZ: EnvironmentVariables
+// - REG_DWORD: TerminateChildProcessesOnServiceStop
+// - REG_DWORD: RestartOnApplicationExit
+// - REG_DWORD: GracefulTerminationTimeout
+// - REG_SZ: StandardOutputRedirectFilePath
+// - REG_SZ: StandardErrorRedirectFilePath
+
 int main()
 {
     std::wprintf(L"Hello World! - RunAsSvc");
